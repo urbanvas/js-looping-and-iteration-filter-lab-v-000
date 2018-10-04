fb91b8b756d920c5634993babb82c4c8dd3fadc7
Neil Seejoor <nvseejoor@gmail.com>
2018-10-04 18:13:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Code your solution in this file
+function findMatching(arr, name) {
+  return arr.filter(person => {
+    return person.toLowerCase() === name.toLowerCase()
+  })
+}
+
+function fuzzyMatch(arr, letters) {
+  return arr.filter(el => el.toLowerCase().indexOf(letters.toLowerCase()) > -1)
+}
